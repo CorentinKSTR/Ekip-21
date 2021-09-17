0570f78e0997cfe890dec97fd4923baecb2f1f58
Arthur <arthur.lucas@edu.devinci.fr>
2021-09-17 18:26:25 +0200

    patch js error

--- override-devinci.js
+++ override-devinci.js
@@ -1,7 +1,7 @@
 var note = document.getElementsByClassName('badge')[0];
 
 
-var body = document.getElementsByTagName('body');
+var body = document.getElementsByTagName('body')[0];
 body.classList.add('important');
 
 for(i=0;i<note.length;i++){
