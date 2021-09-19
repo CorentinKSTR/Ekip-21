8afeea1cd32c456db25246e897143f846075b1c0
Corentin <corentin.kistler@edu.devinci.fr>
2021-09-19 13:55:10 +0200

    :sparkles: commit for merge

--- override-devinci.js
+++ override-devinci.js
@@ -1,9 +1,9 @@
-var note = document.getElementsByClassName('badge')[0];
+var note = document.getElementsByClassName('badge');
 
-
-var body = document.getElementsByTagName('body');
+var body = document.getElementsByTagName('body')[0];
 body.classList.add('important');
 
+
 for(i=0;i<note.length;i++){
-    note.classList.add('note')
+    note[i].classList.add('note')
 }
